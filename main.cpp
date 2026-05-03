fd126823315c477594a339cb39d2fc16e4644120
Mrpasta01 <49362161+mrpasta01@users.noreply.github.com>
2026-05-04 03:30:58 +0700

    v2

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include "resource.h"
 #include <vector>
 #include <string>
-
 #pragma comment(lib, "uxtheme.lib")
 #pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
@@ -154,31 +153,138 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
+// Функция для выполнения действия по номеру
+int ExecuteByNumber(int number, HWND hwnd) {
+    switch (number) {
+    case 1:
+        CleanTempFiles();
+        MessageBox(hwnd, L"Temp очищен!", L"Success", MB_OK);
+        return 1;
+    case 2: {
+        // Системная очистка диска
+        // Для простоты - без настройки категорий
+        RunCommand(L"cleanmgr.exe", L"");
+        MessageBox(hwnd, L"Запущена очистка диска", L"Success", MB_OK);
+        return 1;
+    }
+    case 3: {
+        std::vector<std::wstring> results = CleanInvalidSoftwareReg();
+        if (!results.empty()) {
+            std::wstring finalLog = L"Удалены следующие ветки:\r\n\r\n";
+            for (const auto& name : results) {
+                finalLog += L"  o  " + name + L"\r\n";
+            }
+            ShowResultsDialog(hwnd, finalLog);
+        }
+        else {
+            MessageBox(hwnd, L"Мусорных записей не обнаружено.", L"Реестр", MB_OK);
+        }
+        return 1;
+    }
+    case 4:
+        RunCommand(L"cmd.exe", L"/k sfc /scannow");
+        MessageBox(hwnd, L"Результат будет написан в консоли", L"Success", MB_OK);
+        return 1;
+    case 5:
+        RunCommand(L"cmd.exe", L"/k chkdsk");
+        MessageBox(hwnd, L"Результат будет написан в консоли", L"Success", MB_OK);
+        return 1;
+    case 6:
+        MessageBox(hwnd, L"Внимание! Требуется интернет. Процесс может занять до 20 минут.", L"DISM Recovery", MB_OK | MB_ICONINFORMATION);
+        RunCommand(L"cmd.exe", L"/k dism /online /cleanup-image /restorehealth");
+        return 1;
+    case 7:
+        RestartExplorer();
+        return 1;
+    case 8:
+        SmartOrganize(hwnd);
+        return 1;
+    default:
+        return 0;
+    }
+}
+
+// Функция для отображения справки в MessageBox (если нужно)
+void ShowHelpDialog(HWND hwnd) {
+    std::wstring helpText =
+        L"CLEANER - Команды командной строки\n\n"
+        L"Cleaner 1 - Очистить временные файлы\n"
+        L"Cleaner 2 - Системная очистка диска\n"
+        L"Cleaner 3 - Очистить реестр\n"
+        L"Cleaner 4 - Проверка целостности (SFC)\n"
+        L"Cleaner 5 - Проверка диска (CHKDSK)\n"
+        L"Cleaner 6 - Глубокое восстановление (DISM)\n"
+        L"Cleaner 7 - Перезапустить Проводник\n"
+        L"Cleaner 8 - Сортировка файлов\n\n"
+        L"Cleaner help / ? - Показать справку\n"
+        L"Cleaner - Запустить графический интерфейс";
+
+    MessageBox(hwnd, helpText.c_str(), L"Справка Cleaner", MB_OK | MB_ICONINFORMATION);
+}
+
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+
+    // Парсим аргументы командной строки
+    std::string cmdLine(lpCmdLine);
+    int argNumber = 0;
+
+    if (!cmdLine.empty()) {
+        // Убираем пробелы в начале
+        size_t start = cmdLine.find_first_not_of(" \t");
+        if (start != std::string::npos) {
+            cmdLine = cmdLine.substr(start);
+            argNumber = std::atoi(cmdLine.c_str());
+        }
+    }
+
     const wchar_t CLASS_NAME[] = L"ModernUtilClass";
     HICON hMainIcon = ExtractIcon(hInstance, L"shell32.dll", 84);
-    WNDCLASS wc = { };
+    WNDCLASS wc = {};
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
-    wc.hbrBackground = (HBRUSH)(COLOR_BTNFACE + 1); 
+    wc.hbrBackground = (HBRUSH)(COLOR_BTNFACE + 1);
     wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.hIcon = hMainIcon; 
+    wc.hIcon = hMainIcon;
     RegisterClass(&wc);
 
     HWND hwnd = CreateWindowEx(0, CLASS_NAME, L"Cleaner",
         WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
         CW_USEDEFAULT, CW_USEDEFAULT, 280, 440, NULL, NULL, hInstance, NULL);
-
+    
+    // Если передан аргумент - выполняем соответствующую функцию
     if (!hwnd) return 0;
 
+    // Проверка help
+    if (cmdLine == "help" || cmdLine == "?") {
+        ShowHelpDialog(hwnd);
+        return 0;
+    }
+
+    int num = atoi(cmdLine.c_str());
+    if (num >= 1 && num <= 8) {
+        ShowWindow(hwnd, SW_HIDE);
+
+        // Выполняем действие
+        ExecuteByNumber(argNumber, hwnd);
+
+        // Даем время на выполнение и закрываемся
+        Sleep(2000);
+        return 0;
+    }
+    
+
+    // Если аргумента нет или он неверный - показываем окно
     ShowWindow(hwnd, nCmdShow);
-    MSG msg = { };
+
+    MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
+
     CoUninitialize();
     return 0;
 }
